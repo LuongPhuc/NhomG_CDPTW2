48af10166436ee13a56da6f47c0a25871e7cc2ee
lethanh99 <lethanh131199@gmail.com>
2019-10-19 17:14:46 +0700

    Lê Thành

---
+++ module8/8-content.php
@@ -0,0 +1,79 @@
+<?php
+// include '../config.php';
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+?>
+<div class="type-1215">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4 col-md-5 col-xs-5 col-lg-5">
+                <h3 class="kth3">Our Newsletter</h3>
+                <hr>
+            </div>
+            <div class="col-sm-8 col-md-7 col-xs-7 col-lg-7">
+
+                <input type="email" name="EMAIL" placeholder="Your email address" required="" class="cssemail">
+                <input type="submit" value="Subscribe" class="csssub">
+
+            </div>
+        </div>
+    </div>
+    <div class="helendo_empty_space_lg hidden-md hidden-sm hidden-xs" style="height: 50px"></div>
+    <div class="helendo_empty_space_lg hidden-md hidden-sm hidden-xs" style="height: 50px"></div>
+    <div class="helendo_empty_space_lg hidden-md hidden-sm hidden-xs" style="height: 49px"></div>
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide ">
+                <img src="./images/module8_h1.PNG" alt="">
+            </div>
+            <div class="swiper-slide">
+                <img src="./images/module8_h2.PNG" alt="">
+            </div>
+            <div class="swiper-slide">
+                <img src="./images/module8_h3.PNG" alt="">
+            </div>
+            <div class="swiper-slide">
+                <img src="./images/module8_h4.PNG" alt="">
+            </div>
+            <div class="swiper-slide">
+                <img src="./images/module8_h5.PNG" alt="">
+            </div>
+            <div class="swiper-slide">
+                <img src="./images/module8_h6.PNG" alt="">
+            </div>
+            <div class="swiper-slide">
+                <img src="./images/module8_h7.PNG" alt="">
+            </div>
+            <div class="swiper-slide">
+                <img src="./images/module8_h8.PNG" alt="">
+            </div>
+            <div class="swiper-slide">
+                <img src="./images/module8_h9.PNG" alt="">
+            </div>
+            <div class="swiper-slide">
+                <img src="./images/module8_h10.PNG" alt="">
+            </div>
+        </div>
+        <!-- Add Arrows -->
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+    </div>
+    <script>
+        var swiper = new Swiper('.swiper-container', {
+            slidesPerView: 6,
+            spaceBetween: 30,
+            slidesPerGroup: 2,
+            loop: true,
+            loopFillGroupWithBlank: true,
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+
+        });
+    </script>
+</div>
---
+++ module8/8.php
@@ -0,0 +1,45 @@
+<?php
+// include '../config.php';
+ 
+        $url_host = 'http://'.$_SERVER['HTTP_HOST'];
+        $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+        $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+        
+        preg_match_all($pattern_uri, __DIR__, $matches);
+        $url_path = $url_host . $matches[1][0];
+        $url_path = str_replace('\\', '/', $url_path);
+if (!class_exists('lessc')) {
+    $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);
+    require_once($dir_block.'/libs/lessc.inc.php');
+}
+$less = new lessc;
+$less->compileFile('less/8.less', 'css/8.css');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <title>Module - 8</title>
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link href="<?php echo $url_path ?>/css/bootstrap-3.3.6.min.css" rel="stylesheet">
+   <link href="<?php echo $url_path ?>/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+   <link href="<?php echo $url_path ?>/css/swiper-3.4.2.min.css" rel="stylesheet" type="text/css"/>
+   <link href="<?php echo $url_path ?>/css/8.css" rel="stylesheet" type="text/css" />
+   <script src="<?php echo $url_path ?>/js/swiper-4.5.0.min.js"></script>
+   
+  
+
+
+</head>
+<body>
+    <?php include $dir_block.'/8-content.php'; ?>
+  
+</body>
+
+</html>
+
+
+
+
